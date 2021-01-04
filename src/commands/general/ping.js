18323d30031037b6e4e46a7f90014d80e3edf97f
Supriyo Kumar Aich <supriyo.developer@gmail.com>
2021-01-04 10:08:39 +0530

    fix intentional error

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -1,20 +1,21 @@
 const { Command } = require('discord-akairo');
 
 class PingCommand extends Command {
-    constructor() {
-        super('ping', {
-            aliases: ['ping', 'pong', 'latency'],
-            category: 'general',
-            cooldown: 2000,
-            channel: 'guild',
-            description: {
-                content: 'Shows API latency.'
-            }
-        })
-    }
-    async exec(message) {alskdjaldkjald
-        return message.util.send(`Pong! 🏓 | ${Math.round(this.client.ws.ping)}ms`);
-    }
+	constructor() {
+		super('ping', {
+			aliases: ['ping', 'pong', 'latency'],
+			category: 'general',
+			cooldown: 2000,
+			channel: 'guild',
+			description: {
+				content: 'Shows API latency.'
+			}
+		});
+	}
+
+	async exec(message) {
+		return message.util.send(`Pong! 🏓 | ${Math.round(this.client.ws.ping)}ms`);
+	}
 }
 
 module.exports = PingCommand;
